d206979ed10e897a055af7d1b46c1006c292c252
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-03-12 07:13:39 +1100

    create base route folder

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
+//import base routes
+//const baseRoutes = require('./routes/base_routes')
+
+
 //Set the view Engine EJS
 app.set('views','./views')
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended:true}))
+
 //Server static files
 app.use(express.static('public'))
+
 //Define home route
 app.get('/', (req, res) => {
     res.render('pages/index')
 })
+
 //Start Server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
