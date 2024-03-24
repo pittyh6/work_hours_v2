48576e8005e36937ff21010e0afcfb2928e3e0d1
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-03-24 15:57:16 +1100

    js carousel

---
+++ public/js/action_style.js
@@ -0,0 +1,45 @@
+/* ------------------ Menu ------------------ */
+const hamburgerMenu = document.querySelector(".icon-menu")
+const showClass = document.querySelector(".nav-items")
+
+//When click icon menu is going to show or hide for the mobile version
+hamburgerMenu.addEventListener("click", function () {
+    if (showClass.classList.contains("show")) {
+        showClass.classList.remove("show")
+    } else {
+        showClass.classList.add("show")
+    }
+})
+/* ------------------ ----- ------------------ */
+
+/* ------------------ Carousel ------------------ */
+const carouselItems = document.querySelectorAll('.carousel-item')
+let currentItem = 0
+
+function showItem(index) {
+    if (index < 0) {
+        index = currentItem.length - 1
+    } else if (index >= carouselItems.length) {
+        index = 0
+    }
+
+    carouselItems.forEach((item, i) => {
+        if (i === index) {
+            item.classList.add('active')
+        } else {
+            item.classList.remove('active')
+        }
+    })
+
+    currentItem = index
+}
+
+function nextItem() {
+    showItem(currentItem + 1)
+}
+function prevItem() {
+    showItem(currentItem - 1)
+}
+setInterval(nextItem, 3000)
+showItem(0)
+/* ------------------ ----- ------------------ */
