2734826aee04eee55d664b6c1a52d5dd9a983665
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-03-28 13:56:10 +1100

    separete files menu and carousel

---
+++ public/js/menu.js
@@ -0,0 +1,13 @@
+/* ------------------ Menu ------------------ */
+const hamburgerMenu = document.querySelector(".icon-menu")
+const showClass = document.querySelector(".nav-items")
+
+//When click icon menu is going to show or hide for the mobile version
+hamburgerMenu.addEventListener("click", function () {
+    if (showClass.classList.contains("show")) {
+        showClass.classList.remove("show")
+    } else {
+        showClass.classList.add("show")
+    }
+})
+/* ------------------ ----- ------------------ */
