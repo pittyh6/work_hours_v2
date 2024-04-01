cf15639d175d4fad431b63e0250679e3f9c5ce10
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-01 16:55:30 +1100

    create schema and ref

--- models/m_time.js
+++ models/m_time.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const staffSchema = new Schema({
-    employeeID: {type: Number, required: true},
+//staff table schema
+const employeeSchema = new Schema({
+    employeeId: {type: Number, required: true},
     employeeName: {type: String,require: true},
     employeePassword: {type:String, require: true},
 });
+// punch hours table schema
+const punchSchema = new Schema({
+    employeeId:{type: Schema.Types.employeeId, ref: 'Employee'},
+    day: {type: String, default: () => moment().format('YYYY-MM-DD')},
+    weekDay: String,
+    punchIn: String,
+    breakStart: String,
+    breakEnd: String,
+    punchOut: String,
+});
+
+// Post table schema 
+const postSchema = new Schema({
+    employeeId: {type: Schema.Types.employeeId, ref: 'Employee'},
+    employeeName: {type: Schema.Types.employeeName, ref: 'Employee'},
+    post: String,
+});
+
+//create the variable reference
+const Employee = mongoose.model('Employee', employeeSchema)
+const Punch = mongoose.model('Punch', punchSchema)
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = {Employee, Punch, Post}
