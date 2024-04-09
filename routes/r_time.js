65e3b76f8f5ffd5e1c3787babf652721c165a899
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-10 07:29:06 +1000

    type string id error

--- routes/r_time.js
+++ routes/r_time.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Time = require('../models/m_time')
 
-const { Employee} = require('../models/m_time')
-const { Punch} = require('../models/m_time')
+const { Employee, Punch } = require('../models/m_time')
 
 const employee = new Employee({
     employeeId: 100002,
@@ -16,12 +15,17 @@ const employee = new Employee({
 router.post('/punchIn', async (req, res) => {
     const { employeeId } = req.body;
     console.log('employeeId: ', employeeId)
-
-    const findEmployeeId = Punch.findOne({ employeeId: employeeId })
-    if (findEmployeeId) {
-        console.log("uhuuuuu.. Employee Id already exist: ", findEmployeeId)
-    } else {
-        console.log('Did not find employee id: ', findEmployeeId.employeeId)
+   
+    try {
+        const findEmployeeId = await Punch.findOne({ employeeId: ememployeeIdployee });
+        if (findEmployeeIdPunch) {
+            console.log("uhuuuuu.. Employee Id already exists: ", findEmployeeId)
+        } else {
+            console.log('Did not find employee id: ', employeeId)
+        }
+    } catch (error) {
+        console.error("Error find employee id in punch table: ", error)
+        res.status(500).send("Internal Server Error.")
     }
 
 })
