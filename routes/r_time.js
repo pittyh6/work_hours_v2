7e33c36efcdc22e3aa9a306ae0a09a8941811ce3
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-15 15:51:15 +1000

    add to db punchIn

--- routes/r_time.js
+++ routes/r_time.js
@@ -29,14 +29,25 @@ const punch = new Punch({
 router.post('/punchIn', async (req, res) => {
     const { employeeId } = req.body
     let todayDate = new Date()
-    let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short'}).format(todayDate)
-    let weekDay = new Intl.DateTimeFormat('en-GB', {weekday: 'long'}).format(todayDate)
-    let hour = new Intl.DateTimeFormat('en-GB', {timeStyle: 'short'}).format(todayDate)
+    let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
+    let weekDay = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
+    let hour = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
     console.log("weekDay: " + weekDay + " day: " + day + " hour: " + hour)
-    
+
     Punch.findOne({ employeeId: employeeId, day: day, punchIn: { $ne: null } }).then((punch) => {
         if (!punch) {
             console.log("Did not find Id in Punch: ", punch)
+            try {
+                Punch.create({
+                    employeeId: employeeId,
+                    day: day,
+                    weekDay: weekDay,
+                    punchIn: hour,
+                })
+            } catch (error) {
+                console.log("Error insertOne: ", error)
+            }
+            console.log("Insert Punch In with success!")
         } else if (punch.punchIn == '') {
             console.log("Punch day: " + todayDate + " Punch In is NULL.")
         } else {
