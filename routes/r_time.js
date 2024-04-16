ed04b91dc20ab3fd577edd7235ef2a3e69999d9b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-16 17:01:26 +1000

    punch and breakStart done

--- routes/r_time.js
+++ routes/r_time.js
@@ -32,7 +32,7 @@ router.post('/punchIn', async (req, res) => {
     let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
     let weekDay = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
     let hour = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
-   
+
     Punch.findOne({ employeeId: employeeId, day: day, punchIn: { $ne: null } }).then((punch) => {
         if (!punch) {
             try {
@@ -41,13 +41,16 @@ router.post('/punchIn', async (req, res) => {
                     day: day,
                     weekDay: weekDay,
                     punchIn: hour,
+                    breakStart: '',
+                    breakEnd: '',
+                    punchOut: '',
                 })
             } catch (error) {
                 console.log("Error add punchIn database: ", error)
             }
             console.log("Insert Punch In with success!")
         } else if (punch.punchIn == '') {
-            console.log(day + " Punch In is NULL.")
+            console.log(day + " Punch In is NULL. There is another punch registered. Punch In can not be registered: ", punch)
         } else {
             console.log(day + " id: " + employeeId + " Punch Already exists")
         }
@@ -55,21 +58,50 @@ router.post('/punchIn', async (req, res) => {
 })
 
 router.post('/breakStart', async (req, res) => {
-    const {employeeId} = req.body
+    const { employeeId } = req.body
     let todayDate = new Date()
-    console.log("todayDate: ", todayDate)
-    
-    Punch.findOne({ employeeId: employeeId, day: todayDate, breakStart: {$ne: null} }).then((foundBreak) => {
-        if(!foundBreak){
-            console.log("Did NOT found id: ", employeeId)
-        }else if(foundBreak.breakStart == ''){
-            console.log(day + " BreakStart In is NULL.")
-        }else{
-            console.log("Founded id: ", foundBreak)
+    let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
+    let weekDay = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
+    let hour = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
+    console.log("todayDate: ", hour)
+    Punch.findOne({ employeeId: employeeId, day: day }).then((punch) => {
+        if (!punch) {
+            try {
+                Punch.create({
+                    employeeId: employeeId,
+                    day: day,
+                    weekDay: weekDay,
+                    punchIn: '',
+                    breakStart: hour,
+                    breakEnd: '',
+                    punchOut: '',
+                })
+            } catch (error) {
+                console.error("Error to register break start: ", error)
+            }
+        } else {
+            Punch.findOne({ employeeId: employeeId, day: day, breakStart: { $ne: null } }).then((foundBreak) => {
+                if (!foundBreak) {
+                    console.log("Did NOT found id: ", employeeId)
+                } else if (foundBreak.breakStart == '') {
+                    console.log(day + " BreakStart In is NULL.")
+                    try{
+                        foundBreak.breakStart = hour
+                        foundBreak.save().then(saveUpdate => {
+                            console.log("Break start updated with success: ", foundBreak)
+                        })
+                    }catch(error){
+                        console.error("Error update break start: ", error)
+                    }
+                    
+                } else {
+                    console.log("Founded break Start registered: ", foundBreak)
+                }
+            })
         }
+
     })
 })
-
 
 
 
