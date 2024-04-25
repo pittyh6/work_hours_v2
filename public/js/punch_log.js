f6aa5160f46062df0cb8024d8014fd218cfe4253
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-25 13:52:58 +1000

    change the return data to ejs file into a function. Start to get the clicked window

--- public/js/punch_log.js
+++ public/js/punch_log.js
@@ -1,44 +1,49 @@
 
 window.onload = async function () {
     if (window.location.href.indexOf('/punch_log') > -1) {
-        // Send the employeeId to the server
-        const response = await fetch(`/api/time/punch_log/${employeeId}`);
-        const workData = await response.json();
-
-        // Update the HTML with the workData
-        const punchLogContainer = document.getElementById('punchLogContainer');
-        punchLogContainer.innerHTML = ''; // Clear previous content
-        workData.forEach(data => {
-            //total calc
-            const punchInTime = new Date(`2024-02-03 ${data.punchIn}`);
-            const punchOutTime = new Date(`2024-02-03 ${data.punchOut}`);
-            const breakStartTime = new Date(`2024-02-03 ${data.breakStart}`);
-            const breakEndTime = new Date(`2024-02-03 ${data.breakEnd}`);
-            const timeDiffInMilliseconds = (punchOutTime - breakEndTime) + (breakStartTime - punchInTime);
-            const totalHours = Math.floor(timeDiffInMilliseconds / (1000 * 60 * 60));
-            const totalMinutes = Math.floor((timeDiffInMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
-            let formattedHours = totalHours.toString().padStart(2, '0');
-            let formattedMinutes = totalMinutes.toString().padStart(2, '0');
-            const formattedTime = `${formattedHours}:${formattedMinutes}`;
-            // create html to show data
-            const logEntry = document.createElement('div');
-            logEntry.classList.add('log-entry');
-            logEntry.innerHTML = `
-                <hr>
-                <p class="day">${data.weekDay} : ${data.day}</p>
-                <p class="clock-in">Clock In: ${data.punchIn}</p>
-                <p class="break-start">Break Start: ${data.breakStart}</p>
-                <p class="break-end">Break End: ${data.breakEnd}</p>
-                <p class="clock-out">Clock Out: ${data.punchOut}</p>
-                <p class="total-day">Total day: ${formattedTime}</p>
-            `;
-            punchLogContainer.appendChild(logEntry);
-        });
+        showData() 
+        
+        getPageNumber()
     }
-    countPunchLog()
 };
+async function showData() {
+    // Send the employeeId to the server
+    const response = await fetch(`/api/time/punch_log/${employeeId}`);
+    const workData = await response.json();
 
-function countPunchLog(){
+    // Update the HTML with the workData
+    const punchLogContainer = document.getElementById('punchLogContainer');
+    punchLogContainer.innerHTML = ''; // Clear previous content
+    workData.forEach(data => {
+        //total calc
+        const punchInTime = new Date(`2024-02-03 ${data.punchIn}`);
+        const punchOutTime = new Date(`2024-02-03 ${data.punchOut}`);
+        const breakStartTime = new Date(`2024-02-03 ${data.breakStart}`);
+        const breakEndTime = new Date(`2024-02-03 ${data.breakEnd}`);
+        const timeDiffInMilliseconds = (punchOutTime - breakEndTime) + (breakStartTime - punchInTime);
+        const totalHours = Math.floor(timeDiffInMilliseconds / (1000 * 60 * 60));
+        const totalMinutes = Math.floor((timeDiffInMilliseconds % (1000 * 60 * 60)) / (1000 * 60));
+        let formattedHours = totalHours.toString().padStart(2, '0');
+        let formattedMinutes = totalMinutes.toString().padStart(2, '0');
+        const formattedTime = `${formattedHours}:${formattedMinutes}`;
+        // create html to show data
+        const logEntry = document.createElement('div');
+        logEntry.classList.add('log-entry');
+        logEntry.innerHTML = `
+        <hr>
+        <p class="day">${data.weekDay} : ${data.day}</p>
+        <p class="clock-in">Clock In: ${data.punchIn}</p>
+        <p class="break-start">Break Start: ${data.breakStart}</p>
+        <p class="break-end">Break End: ${data.breakEnd}</p>
+        <p class="clock-out">Clock Out: ${data.punchOut}</p>
+        <p class="total-day">Total day: ${formattedTime}</p>
+    `;
+        punchLogContainer.appendChild(logEntry);
+    });
+    countPunchLog()
+}
+
+function countPunchLog() {
     const pagination = document.getElementById('pagination')
     const logEntry = document.querySelectorAll('.log-entry');
     const itemPerPage = 7
@@ -46,15 +51,20 @@ function countPunchLog(){
     console.log('log entry count: ', numberPages)
 
     //create the pages number at the botton
-    for(let i = 1; i<= numberPages; i++){
+    for (let i = 1; i <= numberPages; i++) {
         console.log('i: ', i)
         const paginationNumber = document.createElement('button')
         paginationNumber.classList.add('pag-number')
         paginationNumber.innerHTML = `${i}`
         pagination.appendChild(paginationNumber)
     }
-
-    //show the last 7 days
-    
-    
+}
+function getPageNumber() {
+    document.addEventListener('click', function (event) {
+        if (event.target.classList == 'pag-number') {
+            console.log('event.target contains pag-number clase: ', event.target)
+        } else {
+            console.log("Does not contains class pag-number: ", event.target)
+        }
+    })
 }
