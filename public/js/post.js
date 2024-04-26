eb1842550e6d634edb7ccb54504a06f8720832a6
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-26 13:25:32 +1000

    add post.js file and get btns

---
+++ public/js/post.js
@@ -0,0 +1,10 @@
+const btn_cancel = document.getElementById('btn-cancel')
+const btn_post = document.getElementById('btn-post')
+
+btn_cancel.addEventListener('click', () =>{
+    console.log("btn cancel was clicked")
+})
+
+btn_post.addEventListener('click', () =>{
+    console.log("btn post was clicked")
+})
