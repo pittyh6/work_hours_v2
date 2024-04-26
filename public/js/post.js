2fb540ec71793364c8fe19985138b7c735540dc6
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-26 13:28:21 +1000

    clean text area text

--- public/js/post.js
+++ public/js/post.js
@@ -1,8 +1,10 @@
 const btn_cancel = document.getElementById('btn-cancel')
 const btn_post = document.getElementById('btn-post')
+const text_post = document.getElementById('text-post')
 
 btn_cancel.addEventListener('click', () =>{
     console.log("btn cancel was clicked")
+    text_post.value = ''
 })
 
 btn_post.addEventListener('click', () =>{
