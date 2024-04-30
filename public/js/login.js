ada94f9f6196378fa28fbdb7b466ad402d7c7e15
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-04-30 15:16:58 +1000

    start clicke submit

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 const btn_login_cancel = document.querySelector('#btn-login-cancel')
+const btn_login_submit = document.querySelector('#btn-login-submit')
 const input_userId = document.querySelector('#userIdLogin')
 const input_password = document.querySelector('#passwordLogin')
 
@@ -7,3 +8,7 @@ btn_login_cancel.addEventListener('click', function () {
     input_password.value = ''
 })
 
+btn_login_submit.addEventListener('click', function () {
+    let userId  = document.getElementById('userIdLogin').value
+    alert('btn_login_submit clicked: ', userId)
+})
