cbf1aea567f9c1188828e934d2ac7e0d5b4d79e1
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-05-02 12:28:09 +1000

    find wmployee in db

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./config/db')
-const Employee = require('./models/m_time').Employee; 
+const Employee = require('./models/m_time').Employee;
 const timeRouter = require('./routes/r_time')
 const app = express()
 
 
 
 //get the id and first name employee to send to header.ejs included in all pages
-app.use( async function (req, res, next) {
-    try{
-        const employee = await Employee.findOne({employeeId: 100001})
+app.use(async function (req, res, next) {
+    try {
+        const employee = await Employee.findOne({ employeeId: 100001 })
         res.locals.employee = employee
         next()
-    }catch(error){
+    } catch (error) {
         console.error(error)
         res.status(500).send("Internal server error header info employee")
     }
@@ -30,11 +30,11 @@ app.use('/login', baseRoutes)
 app.use('/punch', baseRoutes)
 
 //Set the view Engine EJS
-app.set('views','./views')
+app.set('views', './views')
 app.set('view engine', 'ejs')
 //parse json bodies
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api/time', timeRouter)
 
 //Server static files
@@ -44,6 +44,15 @@ app.use(express.static('public'))
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     console.log('Received login request:', username, password);
+    const employeeData = Employee.findOne({ employeeId: username, employeePassword: password }).then((found) => {
+        if (found) {
+            console.log("employee founded: ", found)
+        }else{
+            console.log("Employee Number or Password Wrong!!!")
+        }
+
+    })
+
     // Handle the login logic here
     res.render('pages/login');
 })
