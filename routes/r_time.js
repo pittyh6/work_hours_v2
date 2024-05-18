fef01240bcde25a812634dad5cd62ae12618e887
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-05-18 11:37:55 +1000

    fixed bug cpunch if person is not logged

--- routes/r_time.js
+++ routes/r_time.js
@@ -42,29 +42,34 @@ router.post('/punchIn', async (req, res) => {
     let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
     let weekDay = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
     let hour = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
-
-    Punch.findOne({ employeeId: employeeId, day: day, punchIn: { $ne: null } }).then((punch) => {
-        if (!punch) {
-            try {
-                Punch.create({
-                    employeeId: employeeId,
-                    day: day,
-                    weekDay: weekDay,
-                    punchIn: hour,
-                    breakStart: '',
-                    breakEnd: '',
-                    punchOut: '',
-                })
-            } catch (error) {
-                console.log("Error add punchIn database: ", error)
+    
+    if(employeeId == ''){
+        console.log("Employee is not logged...")
+    }else{
+        Punch.findOne({ employeeId: employeeId, day: day, punchIn: { $ne: null } }).then((punch) => {
+            if (!punch) {
+                try {
+                    Punch.create({
+                        employeeId: employeeId,
+                        day: day,
+                        weekDay: weekDay,
+                        punchIn: hour,
+                        breakStart: '',
+                        breakEnd: '',
+                        punchOut: '',
+                    })
+                } catch (error) {
+                    console.log("Error add punchIn database: ", error)
+                }
+                console.log("Insert Punch In with success!")
+            } else if (punch.punchIn == '') {
+                console.log(day + " Punch In is NULL. There is another punch registered. Punch In can not be registered: ", punch)
+            } else {
+                console.log(day + " id: " + employeeId + " Punch Already exists")
             }
-            console.log("Insert Punch In with success!")
-        } else if (punch.punchIn == '') {
-            console.log(day + " Punch In is NULL. There is another punch registered. Punch In can not be registered: ", punch)
-        } else {
-            console.log(day + " id: " + employeeId + " Punch Already exists")
-        }
-    })
+        })
+    }
+    
 })
 
 router.post('/breakStart', async (req, res) => {
@@ -73,40 +78,45 @@ router.post('/breakStart', async (req, res) => {
     let day = new Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
     let weekDay = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
     let hour = new Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
-    Punch.findOne({ employeeId: employeeId, day: day }).then((punch) => {
-        if (!punch) {
-            try {
-                Punch.create({
-                    employeeId: employeeId,
-                    day: day,
-                    weekDay: weekDay,
-                    punchIn: '',
-                    breakStart: hour,
-                    breakEnd: '',
-                    punchOut: '',
-                })
-            } catch (error) {
-                console.error("Error to register break start: ", error)
-            }
-        } else {
-            Punch.findOne({ employeeId: employeeId, day: day, breakStart: { $ne: null } }).then((foundBreak) => {
-                if (!foundBreak) {
-                    console.log("Did NOT found id: ", employeeId)
-                } else if (foundBreak.breakStart == '' && foundBreak.breakEnd == '' && foundBreak.punchOut == '') {
-                    try {
-                        foundBreak.breakStart = hour
-                        foundBreak.save().then(saveUpdate => {
-                            console.log("Break start updated with success: ", foundBreak)
-                        })
-                    } catch (error) {
-                        console.error("Error update break start: ", error)
-                    }
-                } else {
-                    console.log("Founded break Start registered: ", foundBreak)
+
+    if(employeeId == ''){
+        console.log("Employee is not logged...")
+    }else{
+        Punch.findOne({ employeeId: employeeId, day: day }).then((punch) => {
+            if (!punch) {
+                try {
+                    Punch.create({
+                        employeeId: employeeId,
+                        day: day,
+                        weekDay: weekDay,
+                        punchIn: '',
+                        breakStart: hour,
+                        breakEnd: '',
+                        punchOut: '',
+                    })
+                } catch (error) {
+                    console.error("Error to register break start: ", error)
                 }
-            })
-        }
-    })
+            } else {
+                Punch.findOne({ employeeId: employeeId, day: day, breakStart: { $ne: null } }).then((foundBreak) => {
+                    if (!foundBreak) {
+                        console.log("Did NOT found id: ", employeeId)
+                    } else if (foundBreak.breakStart == '' && foundBreak.breakEnd == '' && foundBreak.punchOut == '') {
+                        try {
+                            foundBreak.breakStart = hour
+                            foundBreak.save().then(saveUpdate => {
+                                console.log("Break start updated with success: ", foundBreak)
+                            })
+                        } catch (error) {
+                            console.error("Error update break start: ", error)
+                        }
+                    } else {
+                        console.log("Founded break Start registered: ", foundBreak)
+                    }
+                })
+            }
+        })
+    }
 })
 
 //Break End
@@ -116,40 +126,45 @@ router.post('/breakEnd', async (req, res) => {
     let day = Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
     let weekDay = Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
     let hour = Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
-    Punch.findOne({ employeeId: employeeId, day: day }).then(punch => {
-        if (!punch) {
-            try {
-                Punch.create({
-                    employeeId: employeeId,
-                    day: day,
-                    weekDay: weekDay,
-                    punchIn: '',
-                    breakStart: '',
-                    breakEnd: hour,
-                    punchOut: '',
-                })
-                console.log("Created break end register: ", punch)
-            } catch (error) {
-                console.error("Error to register break start: ", error)
-            }
-        } else {
-            Punch.findOne({ employeeId: employeeId, day: day, breakEnd: { $ne: null } }).then(foundBreak => {
-                if (foundBreak.breakEnd == '' && foundBreak.punchOut == '') {
-                    try {
-                        foundBreak.breakEnd = hour
-                        foundBreak.save().then(breakSave => {
-                            console.log("Break End Updated with success. ", foundBreak)
-                        })
-                    } catch (error) {
-                        console.error("Error update break end: ", error)
-                    }
-                } else {
-                    console.log("break End already exists: ", punch)
-                }
 
-            })
-        }
-    })
+    if(employeeId == ''){
+        console.log("Employee is not logged...")
+    }else{
+        Punch.findOne({ employeeId: employeeId, day: day }).then(punch => {
+            if (!punch) {
+                try {
+                    Punch.create({
+                        employeeId: employeeId,
+                        day: day,
+                        weekDay: weekDay,
+                        punchIn: '',
+                        breakStart: '',
+                        breakEnd: hour,
+                        punchOut: '',
+                    })
+                    console.log("Created break end register: ", punch)
+                } catch (error) {
+                    console.error("Error to register break start: ", error)
+                }
+            } else {
+                Punch.findOne({ employeeId: employeeId, day: day, breakEnd: { $ne: null } }).then(foundBreak => {
+                    if (foundBreak.breakEnd == '' && foundBreak.punchOut == '') {
+                        try {
+                            foundBreak.breakEnd = hour
+                            foundBreak.save().then(breakSave => {
+                                console.log("Break End Updated with success. ", foundBreak)
+                            })
+                        } catch (error) {
+                            console.error("Error update break end: ", error)
+                        }
+                    } else {
+                        console.log("break End already exists: ", punch)
+                    }
+    
+                })
+            }
+        })
+    }
 })
 
 
@@ -160,36 +175,39 @@ router.post('/clockOut', async (req, res) => {
     let day = Intl.DateTimeFormat('en-GB', { dateStyle: 'short' }).format(todayDate)
     let weekDay = Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(todayDate)
     let hour = Intl.DateTimeFormat('en-GB', { timeStyle: 'short' }).format(todayDate)
-
-    Punch.findOne({ employeeId: employeeId, day: day }).then(punch => {
-        if (!punch) {
-            Punch.create({
-                employeeId: employeeId,
-                day: day,
-                weekDay: weekDay,
-                punchIn: '',
-                breakStart: '',
-                breakEnd: '',
-                punchOut: hour,
-            })
-            console.log("Clock Out created with success: ")
-        } else {
-            Punch.findOne({ employeeId: employeeId, day: day, punchOut: { $ne: null } }).then(foundClockOut => {
-                try {
-                    if (foundClockOut.punchOut == '') {
-                        foundClockOut.punchOut = hour
-                        foundClockOut.save().then(saveClockOut => {
-                            console.log('Clock Out Updated with success. ', saveClockOut)
-                        })
-                    } else {
-                        console.error("Clock Out Already exists: ", foundClockOut)
+    if(employeeId == ''){
+        console.log("Employee is not logged...")
+    }else{
+        Punch.findOne({ employeeId: employeeId, day: day }).then(punch => {
+            if (!punch) {
+                Punch.create({
+                    employeeId: employeeId,
+                    day: day,
+                    weekDay: weekDay,
+                    punchIn: '',
+                    breakStart: '',
+                    breakEnd: '',
+                    punchOut: hour,
+                })
+                console.log("Clock Out created with success: ")
+            } else {
+                Punch.findOne({ employeeId: employeeId, day: day, punchOut: { $ne: null } }).then(foundClockOut => {
+                    try {
+                        if (foundClockOut.punchOut == '') {
+                            foundClockOut.punchOut = hour
+                            foundClockOut.save().then(saveClockOut => {
+                                console.log('Clock Out Updated with success. ', saveClockOut)
+                            })
+                        } else {
+                            console.error("Clock Out Already exists: ", foundClockOut)
+                        }
+                    } catch (error) {
+                        console.error("Error update Clock Out: ", error)
                     }
-                } catch (error) {
-                    console.error("Error update Clock Out: ", error)
-                }
-            })
-        }
-    })
+                })
+            }
+        })
+    }
 })
 /* ---------------------- --------- ---------------------- */
 
